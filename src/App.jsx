5fd54384be9bd1faa86cb405b8d6b63981919948
Subhadeep Paul <deepsp2001@gmail.com>
2022-03-01 21:21:40 +0530

    add a header component

--- src/App.jsx
+++ src/App.jsx
@@ -3,53 +3,53 @@ import Table from "./Table";
 /*Add import statement here*/
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      buttonClicked: "",
-      assignments: [] /*Below this line, add the students state variable*/,
-      grades: {}
-    };
+		this.state = {
+			buttonClicked: "",
+			assignments: [] /*Below this line, add the students state variable*/,
+			grades: {}
+		};
 
-    this.handleButtonClicked = this.handleButtonClicked.bind(this);
-    this.addAssignment = this.addAssignment.bind(this);
-    /*Uncomment the line below to bind the method*/
-    /*this.addStudent = this.addStudent.bind(this);*/
-    this.addGrade = this.addGrade.bind(this);
-  }
+		this.handleButtonClicked = this.handleButtonClicked.bind(this);
+		this.addAssignment = this.addAssignment.bind(this);
+		/*Uncomment the line below to bind the method*/
+		/*this.addStudent = this.addStudent.bind(this);*/
+		this.addGrade = this.addGrade.bind(this);
+	}
 
-  handleButtonClicked(buttonName) {
-    this.setState({
-      buttonClicked: buttonName
-    });
-  }
+	handleButtonClicked(buttonName) {
+		this.setState({
+			buttonClicked: buttonName
+		});
+	}
 
-  /*Check out this addAssignment method*/
-  addAssignment(assignmentName) {
-    this.setState({
-      assignments: this.state.assignments.concat(assignmentName)
-    });
-  }
+	/*Check out this addAssignment method*/
+	addAssignment(assignmentName) {
+		this.setState({
+			assignments: this.state.assignments.concat(assignmentName)
+		});
+	}
 
-  /*Write an addStudent method here*/
+	/*Write an addStudent method here*/
 
-  addGrade(assignment, student, score) {
-    let grades = this.state.grades;
-    let assignmentName = assignment;
-    let studentName = student;
-    if (!(assignment in grades)) {
-      grades[assignmentName] = {};
-    }
-    grades[assignmentName][studentName] = score;
-    this.setState({ grades: grades });
-  }
+	addGrade(assignment, student, score) {
+		let grades = this.state.grades;
+		let assignmentName = assignment;
+		let studentName = student;
+		if (!(assignment in grades)) {
+			grades[assignmentName] = {};
+		}
+		grades[assignmentName][studentName] = score;
+		this.setState({ grades: grades });
+	}
 
-  render() {
-    let tabChoice = <div />;
+	render() {
+		let tabChoice = <div />;
 
-    /*Uncomment below to render assignments*/
-    /*if (this.state.buttonClicked === "assignments") {
+		/*Uncomment below to render assignments*/
+		/*if (this.state.buttonClicked === "assignments") {
       tabChoice = (
         <List
           placeholder="Add Assignment..."
@@ -60,9 +60,9 @@ class App extends React.Component {
       );
     }*/
 
-    /* Change below to render students*/
+		/* Change below to render students*/
 
-    /*if (this.state.buttonClicked === "students") {
+		/*if (this.state.buttonClicked === "students") {
       tabChoice = (
         <List
           placeholder="Add Assignment..." 
@@ -73,8 +73,8 @@ class App extends React.Component {
       );
     }*/
 
-    /* Uncomment lines below to render grades*/
-    /*if (this.state.buttonClicked === "grades") {
+		/* Uncomment lines below to render grades*/
+		/*if (this.state.buttonClicked === "grades") {
       tabChoice = (
         <Table
           tableNames={this.state.assignments}
@@ -85,39 +85,28 @@ class App extends React.Component {
       );
     }*/
 
-    return (
-      <div>
-        <div className="Box Box--spacious f4">
-          <div className="Box-header">
-            {/* Replace this line with the proper header code*/}
-          </div>
-        </div>
-        <nav className="UnderlineNav d-flex flex-justify-center">
-          <div className="UnderlineNav-body pt-6">
-            <button
-              className="btn btn-primary"
-              onClick={() => this.handleButtonClicked("assignments")}
-            >
-              Assignments
-            </button>
-            <button
-              className="btn btn-primary"
-              onClick={() => this.handleButtonClicked("students")}
-            >
-              Students
-            </button>
-            <button
-              className="btn btn-primary"
-              onClick={() => this.handleButtonClicked("grades")}
-            >
-              Grades
-            </button>
-          </div>
-        </nav>
-        {tabChoice}
-      </div>
-    );
-  }
+		return (
+			<div>
+				<div className="Box Box--spacious f4">
+					<div className="Box-header">{<h3 className="Box-title d-flex flex-justify-center">GradeBook</h3>}</div>
+				</div>
+				<nav className="UnderlineNav d-flex flex-justify-center">
+					<div className="UnderlineNav-body pt-6">
+						<button className="btn btn-primary" onClick={() => this.handleButtonClicked("assignments")}>
+							Assignments
+						</button>
+						<button className="btn btn-primary" onClick={() => this.handleButtonClicked("students")}>
+							Students
+						</button>
+						<button className="btn btn-primary" onClick={() => this.handleButtonClicked("grades")}>
+							Grades
+						</button>
+					</div>
+				</nav>
+				{tabChoice}
+			</div>
+		);
+	}
 }
 
 export default App;
